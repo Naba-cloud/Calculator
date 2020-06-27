6eefdaa18e1bcfc16bef27c83e283aee56a120cd
Naba-cloud <59452017+Naba-cloud@users.noreply.github.com>
2020-06-27 16:20:27 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,120 @@
+function sevn(val)
+{
+
+   document.getElementById("display").value+=parseInt(val);
+}
+
+function eght(val)
+{
+
+   document.getElementById("display").value+=parseInt(val);
+}
+
+
+function nin(val)
+{
+
+   document.getElementById("display").value+=parseInt(val);
+}
+
+function pls(val)
+{
+
+   document.getElementById("display").value+=val;
+}
+function fur(val)
+{
+
+   document.getElementById("display").value+=parseInt(val);
+}
+
+function fve(val)
+{
+
+   document.getElementById("display").value+=parseInt(val);
+}
+
+function sx(val)
+{
+
+   document.getElementById("display").value+=parseInt(val);
+}
+function mnus(val)
+{
+
+   document.getElementById("display").value+=val;
+}
+
+function one(val)
+{
+
+   document.getElementById("display").value+=parseInt(val);
+
+}
+
+function two(val)
+{
+
+   document.getElementById("display").value+=parseInt(val);
+}
+
+function three(val)
+{
+
+   document.getElementById("display").value+=parseInt(val);
+}
+
+function mul(val)
+{
+
+   document.getElementById("display").value+=val;
+}
+function zero(val)
+{
+    document.getElementById("display").value+=val;
+
+}
+function decimal(val)
+{
+
+   document.getElementById("display").value+=val;
+}
+
+function equals(val)
+{
+   
+var result=document.getElementById("display");
+result.value=eval(result.value);
+
+}
+
+function divide(val)
+{
+
+   document.getElementById("display").value+=val;
+}
+function clears(val)
+{
+  document.getElementById("display").value="";
+
+
+}
+
+function del(value)
+{
+  
+ 
+     value = document.getElementById("display").value;
+     document.getElementById("display").value = value.substr(0, value.length - 1);
+
+   
+}
+
+function remainder(val)
+{ 
+    document.getElementById("display").value+=val;
+}
+function sine(val)
+{ 
+    document.getElementById("display").value+=(val);
+}
